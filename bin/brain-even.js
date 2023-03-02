521fa88f2721ecbb7dbc83a35bea8ac569fcf4be
Palina Chuiko <palinachuiko@Palinas-MacBook-Air.local>
2023-03-02 22:34:29 +0100

    new Brain-ever.js file

---
+++ bin/brain-even.js
@@ -0,0 +1,24 @@
+import {nameQuestion} from "/Users/palinachuiko/frontend-project-lvl1/src/cli.js";
+import {myQuest} from "/Users/palinachuiko/frontend-project-lvl1/src/index.js";
+let name = nameQuestion();
+console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+const randomInteger = () => {
+	return Math.floor(Math.random() * 100000);
+};
+
+const evenOrOdd = () => {
+	let mistakeDetected = false;
+	for (let i = 0; i < 3; i++){
+		let number = randomInteger();
+		let correctResult = number % 2 === 0 ? "yes" : "no";
+		mistakeDetected = myQuest(number,correctResult,name);
+		if (mistakeDetected === true) {
+			break;
+		}
+	}
+	if (mistakeDetected === false) {
+		console.log("Congratulations,",name,"!");
+	}			
+};
+evenOrOdd();
