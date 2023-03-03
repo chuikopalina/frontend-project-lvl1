715d4ee43cc2b711a73e7bff93845728e054c05c
Palina Chuiko <palinachuiko@Palinas-MacBook-Air.local>
2023-03-03 15:56:18 +0100

    updated

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,4 +2,4 @@
 //console.log("Welcome to the Brain Games!");
 import {nameQuestion} from "../src/cli.js";
 let name = nameQuestion();
-console.log("Hello, " + name);
+console.log("Hello, " + name,"!");
