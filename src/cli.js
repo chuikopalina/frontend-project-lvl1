4060e7d51279047194ab29b50cf78be0276fc7a5
Palina Chuiko <palinachuiko@Palinas-MacBook-Air.local>
2023-03-03 10:10:34 +0100

    solution for brain-games

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from "readline-sync";
 export const nameQuestion = () => {
 	console.log("Welcome to the Brain Games!");
 	const name = readlineSync.question("May I have your name?");
-	console.log("Hello," + name);
+	//console.log("Hello," + name);
 	return name;
 };
